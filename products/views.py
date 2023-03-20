562b98ffa1913e3b6f169a62a27d9a9c3c360903
abiton198 <abitonp@gmail.com>
2023-03-20 18:12:15 +0200

    feat: attribute of class model

--- products/views.py
+++ products/views.py
@@ -1,7 +1,10 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(requests):
+def index(request):
     return HttpResponse('Welcome, to the new Django Project')
+
+def new(request):
+    return HttpResponse('This is my new pen shop!')
 
 # Create your views here.
