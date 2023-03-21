6916f6d23d6aa30f3e270f901b7d38be683082a4
abiton198 <abitonp@gmail.com>
2023-03-21 16:02:51 +0200

    feat: added admin management

--- products/models.py
+++ products/models.py
@@ -7,5 +7,9 @@ class Product(models.Model):
     stock = models.IntegerField()
     image_url = models.CharField(max_length=2083)
 
+class Offer(models.Model):
+    code = models.CharField(max_length=7)
+    description = models.CharField(max_length=200)
+    discount = models.FloatField()
 
 # Create your models here.
